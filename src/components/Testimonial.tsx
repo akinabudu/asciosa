cc848b9f3f254ec3b2dd5b6809db8b6b88d1b2ef
akin <orglobalng@gmail.com>
2024-04-19 21:35:28 +0100

    change-images

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { motion } from "framer-motion";
+import Image from "next/image";
+import LogoImage  from "../../public/logo.jpg"
 
 const ScrollingTestimonials = () => {
   return (
@@ -9,8 +11,7 @@ const ScrollingTestimonials = () => {
           Testimonials  
         </h3>
         <p className="text-center text-slate-300 text-sm mt-2 max-w-lg mx-auto">
-          Hear from us!
-        </p>
+What we have to say about our College.        </p>
       </div>
       <div className="p-4 overflow-x-hidden relative">
         <div className="absolute top-0 bottom-0 left-0 w-24 z-10 bg-gradient-to-r from-slate-900 to-transparent" />
@@ -58,7 +59,7 @@ const TestimonialList = ({ list, reverse = false, duration = 50 }:TestimonialPro
           key={testimonial.id}
           className="shrink-0 w-[500px] grid grid-cols-[7rem,_1fr] rounded-lg overflow-hidden relative"
         >
-          <img src={testimonial.img} alt="Testimonial" className="w-full h-44 object-cover" />
+          <Image src={testimonial.img} alt="Testimonial" className="w-full h-44 object-cover" />
           <div className="bg-slate-900 text-slate-50 p-4">
             <span className="block font-semibold text-lg mb-1">{testimonial.name}</span>
             <span className="block mb-3 text-sm font-medium">{testimonial.title}</span>
@@ -77,43 +78,38 @@ const testimonials = {
   top: [
     {
       id: 1,
-      img: "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", // Replace with actual image URL
+      img: LogoImage, // Replace with actual image URL
       name: "Jen S.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur esse corporis!",
     },
     {
       id: 2,
-      img: "https://images.unsplash.com/photo-1627161683077-e34782c24d81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=703&q=80", // Replace with actual image URL
-      name: "Paul A.",
+      img: LogoImage,      name: "Paul A.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis.",
     },
     {
       id: 3,
-      img: "https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Cindy J.",
+      img: LogoImage,      name: "Cindy J.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam.",
     },
     {
       id: 4,
-      img: "https://images.unsplash.com/photo-1595211877493-41a4e5f236b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80", // Replace with actual image URL
-      name: "Danica W.",
+      img: LogoImage,      name: "Danica W.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor.",
     },
     {
       id: 5,
-      img: "https://plus.unsplash.com/premium_photo-1670588776139-da93b47afc6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Peter H.",
+      img: LogoImage,      name: "Peter H.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore.",
     },
     {
       id: 6,
-      img: "https://images.unsplash.com/photo-1629425733761-caae3b5f2e50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Lanny B.",
+      img: LogoImage,      name: "Lanny B.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur esse!",
     },
@@ -122,43 +118,37 @@ const testimonials = {
   middle: [
     {
       id: 1,
-      img: "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", // Replace with actual image URL
-      name: "Alex F.",
+      img: LogoImage,      name: "Alex F.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam.",
     },
     {
       id: 2,
-      img: "https://images.unsplash.com/photo-1629425733761-caae3b5f2e50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Claude O.",
+      img: LogoImage,      name: "Claude O.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt.",
     },
     {
       id: 3,
-      img: "https://images.unsplash.com/photo-1627161683077-e34782c24d81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=703&q=80", // Replace with actual image URL
-      name: "Max Q.",
+      img: LogoImage,      name: "Max Q.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis.",
     },
     {
       id: 4,
-      img: "https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Jeff R.",
+      img: LogoImage,      name: "Jeff R.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur esse corporis!",
     },
     {
       id: 5,
-      img: "https://plus.unsplash.com/premium_photo-1670588776139-da93b47afc6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Kevin K.",
+      img: LogoImage,      name: "Kevin K.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit!",
     },
     {
       id: 6,
-      img: "https://images.unsplash.com/photo-1595211877493-41a4e5f236b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80", // Replace with actual image URL
-      name: "Andrea B.",
+      img: LogoImage,      name: "Andrea B.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere!",
     },
@@ -168,43 +158,37 @@ const testimonials = {
   bottom: [
     {
       id: 1,
-      img: "https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Danny G.",
+      img: LogoImage,      name: "Danny G.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur!",
     },
     {
       id: 2,
-      img: "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", // Replace with actual image URL
-      name: "Ian D.",
+      img: LogoImage,      name: "Ian D.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere.",
     },
     {
       id: 3,
-      img: "https://plus.unsplash.com/premium_photo-1670588776139-da93b47afc6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Ben S.",
+      img: LogoImage,      name: "Ben S.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
     },
     {
       id: 4,
-      img: "https://images.unsplash.com/photo-1595211877493-41a4e5f236b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80", // Replace with actual image URL
-      name: "Matthew I.",
+      img: LogoImage,      name: "Matthew I.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur esse corporis!",
     },
     {
       id: 5,
-      img: "https://images.unsplash.com/photo-1627161683077-e34782c24d81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=703&q=80", // Replace with actual image URL
-      name: "Garrett P.",
+      img: LogoImage,      name: "Garrett P.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia.",
     },
     {
       id: 6,
-      img: "https://images.unsplash.com/photo-1629425733761-caae3b5f2e50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", // Replace with actual image URL
-      name: "Xavier C.",
+      img: LogoImage,      name: "Xavier C.",
       title: "Founder of XYZ",
       info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur.",
     },
